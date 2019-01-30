0f84134f148a278becc0681f8c97f11d251783d1
Magesh <tkmagesh77@gmail.com>
2019-01-30 11:21:03 +0530

    using stateManager with bugTracker - adding new bugs

---
+++ 03-bug-tracker/stateManager.js
@@ -0,0 +1,52 @@
+var SM = (function(){
+	var _currentState = undefined,
+		_listeners = [],
+		_reducer = undefined,
+		_init_action = '@@INIT/ACTION';
+
+
+	function getState(){
+		return _currentState;
+	}
+
+	function subscribe(listenerFn){
+		_listeners.push(listenerFn);
+	}
+
+	function triggerChange(){
+		_listeners.forEach(listenerFn => listenerFn());
+	}
+
+	function dispatch(action){
+		var newState = _reducer(_currentState, action);
+		if (newState === _currentState) return;
+		_currentState = newState;
+		triggerChange();
+	}
+
+	function createStore(reducer){
+		_reducer = reducer;
+		_currentState = _reducer(undefined, _init_action);
+		return {
+			getState,
+			subscribe,
+			dispatch
+		};
+	}
+
+	function bindActionCreators(actionCreators, dispatch){
+		let result = {};
+		for(let key in actionCreators){
+			result[key] = function(){
+				let action = actionCreators[key].apply(undefined, arguments);
+				dispatch(action);
+			}
+		}
+		return result;
+	}
+
+	return {
+		createStore,
+		bindActionCreators
+	}
+})();
